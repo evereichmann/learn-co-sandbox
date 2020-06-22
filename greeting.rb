a513abc991750614d288f7b6a34267c6c7107c99
Eve Reichmann <ereichmann4@aol.com>
2020-06-22 16:08:30 +0000

    Automatically backed up by Learn

---
+++ greeting.rb
@@ -0,0 +1,17 @@
+def greeting 
+  puts "hello world"
+end
+
+def say_greeting_five_times
+  greeting
+  greeting
+  greeting
+  greeting
+  greeting
+end
+
+def greeting_a_person(name)
+  puts "hello #{name}"
+end
+
+greeting_a_person "eve"
